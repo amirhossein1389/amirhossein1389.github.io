b27a790281061ee437905c28af5dc696493a7eac
amirhossein1389 <139352128+amirhossein1389@users.noreply.github.com>
2023-07-19 04:48:55 -0700

    Add files via upload

---
+++ 04-5(1)/04-5/script.js
@@ -0,0 +1,3 @@
+let width=document.getElementById('box').clientWidth, height=document.getElementById('box').clientHeight, w=document.getElementById('w'), h=document.getElementById('h');
+w.innerHTML = width;
+h.innerHTML = height;
